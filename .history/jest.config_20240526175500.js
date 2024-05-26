122ed8fea91e663d9f0b6b270224fae65cd21ce9
amiranazri <amira@lifelinelab.io>
2024-05-26 17:57:28 +0800

    Updated jest config file and added gitignore file

--- .history/jest.config_20240526175500.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  testMatch: ["**/*.test.js"],
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "html"],
-};
